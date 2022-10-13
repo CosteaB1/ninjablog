d3d2bbfd2750aa9c720b23f184bfa496338a4512
Bivol Constantin <bivolcostea97@gmail.com>
2022-10-13 23:32:02 +0300

    First 5 lessons xD

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = 'http://www.google.com';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content"></div>
+      <h1>{title}</h1>
+      <p>Liked {likes} time</p>
+      {/* dynamic variable  */}
+
+      <p>{10}</p>
+      <p>{'Hello, ninjas'}</p>
+      <p>{[1,2,3,4,5]}</p>
+      <p>{Math.random() * 10}</p>
+      <a href={link}>Google Site</a>
     </div>
   );
 }
 
-export default App;
+export default App; //we use export so we can use this component in other files. It is a must!
